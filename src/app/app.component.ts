d08683f1882933d93a1d797a6d94a606cd658380
Gajendrasinh Zala <gajendrasinh.zala93@gmail.com>
2019-08-31 01:34:32 +0800

    final code

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component} from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+
+  data: any[] = [];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 10; i++) {
+      this.data.push(this.createRandomItem());
+    }
+    console.log(JSON.stringify(this.data))
+  }
+
+  setAlarm(status) {
+    return status == 'alarm' ? true : false;
+  }
+
+  createItem(index, randomStatus) {
+    return { id: index, name: `Item ${index}`, details: `Status: ${randomStatus}`, status: randomStatus };
+  }
+  createRandomItem() {
+    const int = parseInt((Math.random() * 100) + '', 10);
+    const randomStatus = Math.random() >= 0.3 ? "normal" : "alarm";
+    return this.createItem(int, randomStatus);
+  }
+
+ 
+}
